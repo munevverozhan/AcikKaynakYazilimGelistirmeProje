173fe8c348bd06564eedfe790ce0764d2f69f2b6
munevverozhan <merveozh@gmail.com>
2022-10-19 01:15:45 +0300

    Public bir API ye windows form ile erişim sağladım ve sorgu atadım. Butona basınca gelen sonucu label a yazdırdım.

---
+++ AcikKaynakYazilimGelistirmeProje/APIForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcikKaynakYazilimGelistirmeProje
+{
+    public partial class APIForm : Form
+    {
+        public APIForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://emojihub.herokuapp.com/");
+            HttpResponseMessage responce = await client.GetAsync("api/random/category_food_and_drink");
+            string result = await responce.Content.ReadAsStringAsync();
+            label1.Text = result;
+        }
+    }
+}
--- AcikKaynakYazilimGelistirmeProje/Program.cs
+++ AcikKaynakYazilimGelistirmeProje/Program.cs
@@ -16,7 +16,7 @@ namespace AcikKaynakYazilimGelistirmeProje
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new APIForm());
         }
     }
 }
